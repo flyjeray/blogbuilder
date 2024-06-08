2c91a6914f13bba9f61c2ce0b6e7c06ffa0c6b37
Daniil <flyjeray.dev@gmail.com>
2024-06-08 17:54:11 +0300

    [front] setup project

---
+++ frontend/blogbuilder/src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+const AdminRootPage = () => {
+  return (
+    <main>
+      <h1>Admin Root Page</h1>
+    </main>
+  )
+}
+
+export default AdminRootPage;
---
+++ frontend/blogbuilder/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>BlogBuilder</h1>
+    </main>
+  );
+}
